480acb6ca473c9f79a656b6715f2ba7e68c51890
Tehn Yit Chin <tehn.yit.chin@gmail.com>
2016-05-15 17:50:47 -0400

    updating ckeditor

--- qa-plugin/wysiwyg-editor/ckeditor/build-config.js
+++ qa-plugin/wysiwyg-editor/ckeditor/build-config.js
@@ -1,5 +1,5 @@
-/**
- * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+﻿/**
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
@@ -13,10 +13,10 @@
  * (1) http://ckeditor.com/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) http://ckeditor.com/builder/902d56a59737d17f7f952c5aeb87d204
+ * (2) http://ckeditor.com/builder/24940abe6060b89d22bfdf5433c4fe68
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) http://ckeditor.com/builder/download/902d56a59737d17f7f952c5aeb87d204
+ * (3) http://ckeditor.com/builder/download/24940abe6060b89d22bfdf5433c4fe68
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -26,7 +26,7 @@
 
 var CKBUILDER_CONFIG = {
 	skin: 'moono',
-	preset: 'standard',
+	preset: 'basic',
 	ignore: [
 		'.bender',
 		'bender.js',
@@ -42,6 +42,7 @@ var CKBUILDER_CONFIG = {
 		'.jscsrc',
 		'.jshintignore',
 		'.jshintrc',
+		'less',
 		'.mailmap',
 		'node_modules',
 		'package.json',
@@ -49,84 +50,20 @@ var CKBUILDER_CONFIG = {
 		'tests'
 	],
 	plugins : {
+		'about' : 1,
 		'basicstyles' : 1,
-		'blockquote' : 1,
 		'clipboard' : 1,
-		'colorbutton' : 1,
-		'colordialog' : 1,
-		'contextmenu' : 1,
+		'enterkey' : 1,
 		'entities' : 1,
-		'filebrowser' : 1,
-		'font' : 1,
-		'format' : 1,
-		'horizontalrule' : 1,
-		'image' : 1,
+		'floatingspace' : 1,
 		'indentlist' : 1,
-		'justify' : 1,
 		'link' : 1,
 		'list' : 1,
-		'liststyle' : 1,
-		'magicline' : 1,
-		'maximize' : 1,
-		'pastefromword' : 1,
-		'removeformat' : 1,
-		'resize' : 1,
-		'smiley' : 1,
-		'specialchar' : 1,
-		'tab' : 1,
-		'table' : 1,
-		'tabletools' : 1,
 		'toolbar' : 1,
 		'undo' : 1,
 		'wysiwygarea' : 1
 	},
 	languages : {
-		'ar' : 1,
-		'bn' : 1,
-		'ca' : 1,
-		'cs' : 1,
-		'cy' : 1,
-		'da' : 1,
-		'de' : 1,
-		'el' : 1,
-		'en' : 1,
-		'en-gb' : 1,
-		'es' : 1,
-		'et' : 1,
-		'fa' : 1,
-		'fi' : 1,
-		'fr' : 1,
-		'he' : 1,
-		'hr' : 1,
-		'hu' : 1,
-		'id' : 1,
-		'is' : 1,
-		'it' : 1,
-		'ja' : 1,
-		'ka' : 1,
-		'km' : 1,
-		'ko' : 1,
-		'ku' : 1,
-		'lt' : 1,
-		'lv' : 1,
-		'nl' : 1,
-		'no' : 1,
-		'pl' : 1,
-		'pt' : 1,
-		'pt-br' : 1,
-		'ro' : 1,
-		'ru' : 1,
-		'sk' : 1,
-		'sl' : 1,
-		'sq' : 1,
-		'sr' : 1,
-		'sr-latn' : 1,
-		'th' : 1,
-		'tr' : 1,
-		'ug' : 1,
-		'uk' : 1,
-		'vi' : 1,
-		'zh' : 1,
-		'zh-cn' : 1
+		'en' : 1
 	}
 };
